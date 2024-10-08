bdb92dabc2542c0c5ef4abe95864544badd8392e
Sonia Matyjaszczyk <aphexsonia@gmail.com>
2024-10-08 17:56:03 +0200

    add M5Z2.py

--- Desktop/Kodilla/M5Z2/M5Z2.py
+++ Desktop/Kodilla/M5Z2/M5Z2.py
@@ -1,13 +1,23 @@
+#podpunkt 1 i 2
 class Film:
-    def __init__ (self, title, year, type, plays_number):
-        self.title = title
-        self.year = year
-        self.type = type
-        self.plays_number = plays_number
+    def __init__ (self, film_title, film_year, film_type, film_plays_number):
+        self.film_title = film_title
+        self.film_year = film_year
+        self.film_type = film_type
+        self.film_plays_number = film_plays_number
 
-class Series(Film):
-    def __init__(self, series_number, sezon_number, *args, **kwargs)
-        super().__init__(*args, **kwargs)
+class Series:
+    def __init__(self, series_title, series_year, series_type, series_plays_number, series_number, sezon_number)
+        self.series_title = series_title
+        self.series_year = series_year
+        self.series_type = series_type
+        self.series_plays_number = series_plays_number
         self.series_number = series_number
         self.sezon_number = sezon_number
+
+
+
+
+#podpunkt 3
+
 
